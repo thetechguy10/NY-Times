f58a4fa046f5b80dd0372cfafbf5f34dbb672d99
thetechguy10 <jaykapadia44444@yahoo.in>
2019-06-28 14:24:19 +0530

    Created design of article and connected the article data to recyclerview and created time function to get the article published date.

--- app/src/main/java/com/jaykapadia/nytimes/MainActivity.java
+++ app/src/main/java/com/jaykapadia/nytimes/MainActivity.java
@@ -1,43 +1,51 @@
 package com.jaykapadia.nytimes;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import android.util.Log;
-import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
-import com.jaykapadia.nytimes.Model.Section;
+import com.jaykapadia.nytimes.Model.Article;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    ArticleViewModel model;
+    article_adapter adapter;
+    ArrayList<Article> articles = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -47,13 +55,98 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        final ArticleViewModel model1;
-        model1 = ViewModelProviders.of(this).get(ArticleViewModel.class);
-        model1.getArticleRepository().observe(this,section1 -> {
-            Log.d("xxxxx", String.valueOf(section1.getNum_results()));
-        });
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+
+        adapter = new article_adapter(this, articles,getDate());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        load();
 
 
+
+
+    }
+    BroadcastReceiver networkChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("app","Network connectivity change");
+        }
+    };
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    private String getDate(){
+        Calendar calNewYork = Calendar.getInstance();
+        calNewYork.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+
+        String date = String.valueOf(calNewYork.get(Calendar.DAY_OF_MONTH));
+        String month =  String.valueOf(calNewYork.get(Calendar.MONTH)+1);
+        String year = String.valueOf(calNewYork.get(Calendar.YEAR));
+        String hour = String.valueOf(calNewYork.get(Calendar.HOUR_OF_DAY));
+        String minute= String.valueOf(calNewYork.get(Calendar.MINUTE));
+        String second = String.valueOf(calNewYork.get(Calendar.SECOND));
+
+        Log.d("xxxxxx",year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second);
+        return year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second;
+    }
+
+
+
+
+    private void load(){
+        if (connectionAvailable()){
+            model = ViewModelProviders.of(this).get(ArticleViewModel.class);
+            model.getArticleRepository().observe(this, section1 -> {
+                        if (section1 == null) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setTitle("Error Fetching Data");
+                            builder.setMessage("Unknown error occured");
+                            builder.setPositiveButton("Retry", (dialog, which) -> load());
+                            builder.setNegativeButton("Cancel",((dialog, which) -> dialog.dismiss()));
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        } else {
+                            List<Article> articleList = section1.getResults();
+                            articles.addAll(articleList);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+            );
+        }else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Connection Problem");
+            builder.setMessage("Please connect to the Internet");
+            builder.setPositiveButton("Retry", (dialog, which) -> load());
+            builder.setNegativeButton("Cancel",((dialog, which) -> {dialog.dismiss();
+                finish();}));
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    private boolean connectionAvailable() {
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Objects.requireNonNull(connectivityManager).getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        }
+        return connected;
     }
 
     @Override
@@ -87,4 +180,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
---
+++ app/src/main/java/com/jaykapadia/nytimes/article_adapter.java
@@ -0,0 +1,98 @@
+package com.jaykapadia.nytimes;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jaykapadia.nytimes.Model.Article;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class article_adapter extends RecyclerView.Adapter<article_adapter.holder> {
+    private Context context;
+    private ArrayList<Article> articles;
+    private String data;
+    article_adapter(Context ctx, ArrayList<Article> articles, String date) {
+        this.context = ctx;
+        this.articles = articles;
+        this.data = date;
+    }
+
+    @NonNull
+    @Override
+    public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return new holder(inflater.inflate(R.layout.article_card, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull holder holder, int position) {
+        holder.t1.setText(articles.get(position).getTitle());
+        holder.t2.setText(articles.get(position).getAbstrac());
+        holder.t3.setText(getTime(articles.get(position).getUpdated_date()));
+        if (articles.get(position).getMultimedia().size()==0){
+            Picasso.get().load(R.drawable.error).into(holder.i1);
+        }else {
+            Picasso.get().load(articles.get(position).getMultimedia().get(1).getUrl()).into(holder.i1);
+        }
+
+    }
+
+    private String getTime(String s){
+        String year = s.substring(0,4);
+        String month = s.substring(5,7);
+        String date = s.substring(8,10);
+        String hour = s.substring(11,13);
+        String minute = s.substring(14,16);
+        String second = s.substring(17,19);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+        String fi = year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second;
+
+        int hours = 0;
+        try {
+            Date date1 = format.parse(data);
+            Date date2 = format.parse(fi);
+            long mills = date1.getTime() - date2.getTime();
+            hours = (int) (mills/(1000 * 60 * 60));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if (hours<24){
+            return  hours+"h ago";
+        }else {
+            return hours /24+ "d ago";
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    class holder extends RecyclerView.ViewHolder {
+        TextView t1, t2,t3;
+        ImageView i1;
+
+        holder(@NonNull View itemView) {
+            super(itemView);
+
+            t1 = itemView.findViewById(R.id.title);
+            t2 = itemView.findViewById(R.id.abstrac);
+            t3 = itemView.findViewById(R.id.time);
+            i1 = itemView.findViewById(R.id.icon);
+        }
+    }
+}
